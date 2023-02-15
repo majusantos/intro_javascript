42c2d2d4f0abbf78229867d763926c4ac7fd4b67
majusantos <124818845+majusantos@users.noreply.github.com>
2023-02-15 17:30:07 -0300

    variaveis

--- exemplos/000-javascript.js
+++ exemplos/000-javascript.js
@@ -1,1 +1,30 @@
+// o comando abaixo apresenta uma mensagem na tela
 console.log('ola estou programando em javascript');
+
+/*
+         comentario que ocupa mais
+         de uma linha
+*/
+
+// criação de variavel do tipo texto
+let nome = 'maju santos';
+// criação de variavel do tipo numerico (inteiro)
+let idade = 16;
+// criação de variavel do tipo numerico (decimal)
+let altura = 1.68;
+// criação de variavel do tipo verdadeiro/falso
+let trabalha = true; // true = verdadeiro | false = falso
+// exemplo de variavel do nome composto 
+let corfavorita = 'laranja';
+// exemplo de variavel do tipo lista de valores 
+let cores_favoritas = [ 'laranja', 'roxo', 'azul'];
+//  exemplo de variavel que armazena um objeto 
+let endereço = {
+    'lougradouro'       :'Rua',
+    'nome_lougradouro'  :'brasil',
+    'número'            :'50 A',
+    'bairro'            :'centro',
+    'cidade'            :'Tupã'
+};
+let covid;
+let dengue = null;
